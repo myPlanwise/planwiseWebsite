060f110703a923480fa31af0649dc2ecb7649932
luu-jennifer <jenniferluu87@gmail.com>
2022-12-03 11:25:30 -0800

    added under construction msg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,11 @@
 const Header = () => {
   return (
     <header>
-      <h1>Welcome to MyPlanWise</h1>
+      <div className="contentContainer">
+        <h1>Welcome to myPlanwise</h1>
+        <p>myPlanwise is under construction. We are working on exciting initiatives to help you plan your future.</p>
+        <p>If you have suggestions or would like to get in connect - email us at hello@myplanwise.com, </p>
+      </div>
       <button>Get Started</button>
     </header>
   )
